d47410d3b66c17f1dfb10e50163679f0ec5571f4
Abubakar Gill <abubakargill@Abubakars-MacBook-Pro-14.local>
2026-02-02 04:57:26 +0500

    Optimize: Stable Build ID and font loading display swap

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    generateBuildId: async () => {
+        // This ensures the build ID is stable between deployments if the version is same
+        return 'disposemail-v1';
+    },
 };
 
 module.exports = nextConfig;
